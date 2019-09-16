c3a24449ecf7c7b12903bc6a24836222ddd9b341
Antony Mulvey <rc0039078@student.riversidecollege.ac.uk>
2019-09-16 15:43:54 +0100

    Add project files.

---
+++ move picture 4/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace move_picture_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Up = new System.Windows.Forms.Button();
+            this.Down = new System.Windows.Forms.Button();
+            this.Left = new System.Windows.Forms.Button();
+            this.Right = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Up
+            // 
+            this.Up.Location = new System.Drawing.Point(85, 12);
+            this.Up.Name = "Up";
+            this.Up.Size = new System.Drawing.Size(75, 23);
+            this.Up.TabIndex = 0;
+            this.Up.Text = "Up";
+            this.Up.UseVisualStyleBackColor = true;
+            this.Up.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Down
+            // 
+            this.Down.Location = new System.Drawing.Point(85, 83);
+            this.Down.Name = "Down";
+            this.Down.Size = new System.Drawing.Size(75, 23);
+            this.Down.TabIndex = 1;
+            this.Down.Text = "Down";
+            this.Down.UseVisualStyleBackColor = true;
+            this.Down.Click += new System.EventHandler(this.Down_Click);
+            // 
+            // Left
+            // 
+            this.Left.Location = new System.Drawing.Point(0, 48);
+            this.Left.Name = "Left";
+            this.Left.Size = new System.Drawing.Size(75, 23);
+            this.Left.TabIndex = 2;
+            this.Left.Text = "Left";
+            this.Left.UseVisualStyleBackColor = true;
+            // 
+            // Right
+            // 
+            this.Right.Location = new System.Drawing.Point(170, 48);
+            this.Right.Name = "Right";
+            this.Right.Size = new System.Drawing.Size(75, 23);
+            this.Right.TabIndex = 3;
+            this.Right.Text = "Right";
+            this.Right.UseVisualStyleBackColor = true;
+            this.Right.Click += new System.EventHandler(this.Right_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(713, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::move_picture_4.Properties.Resources.fast_car_2;
+            this.pictureBox1.Location = new System.Drawing.Point(344, 211);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Right);
+            this.Controls.Add(this.Left);
+            this.Controls.Add(this.Down);
+            this.Controls.Add(this.Up);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Up;
+        private System.Windows.Forms.Button Down;
+        private System.Windows.Forms.Button Left;
+        private System.Windows.Forms.Button Right;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ move picture 4/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace move_picture_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            {
+                if (pictureBox1.Location.Y + 0 < ClientRectangle.Width - pictureBox1.Width)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.Y - 25, pictureBox1.Location.Y);
+                }
+            }
+        }
+
+        private void Down_Click(object sender, EventArgs e)
+        {
+            {
+                if (pictureBox1.Location.Y + 0 < ClientRectangle.Width - pictureBox1.Width)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.Y + 25, pictureBox1.Location.Y);
+                }
+            }
+        }
+
+        private void Right_Click(object sender, EventArgs e)
+        {
+            {
+                if (pictureBox1.Location.X + 25 < ClientRectangle.Width - pictureBox1.Width)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 25, pictureBox1.Location.Y);
+                }
+            }
+        }
+    }
+}
